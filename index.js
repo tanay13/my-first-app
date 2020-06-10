54b62ef5b70219c7c2dea76137ba337dc8786dbc
Tanay Raj <tanay.raj76@gmail.com>
2020-06-10 15:32:47 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,19 +1,60 @@
-/**
- * This is the main entrypoint to your Probot app
- * @param {import('probot').Application} app
- */
-module.exports = app => {
-  // Your code here
-  app.log('Yay, the app was loaded!')
+module.exports = (app) => {
+  const adminUsernames = [];
 
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
-  })
+  app.log("Yay, the bot was loaded!");
+  app.on("issues.opened", async (context) => {
+    const issue = context.payload.issue;
+    if (adminUsernames.includes(issue.user.login)) {
+      app.log(
+        `Ignoring new issue ${issue.id} created by admin ${issue.user.login}`
+      );
+      return;
+    }
+    if (!issue.closed_at) {
+      app.log(`Issue Opened: ${issue.id}`);
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+      const comment = context.issue({
+        body: `Thanks @${issue.user.login}, for raising the issue!  🙌
+  One of our team mates will revert on this soon. ✅`,
+      });
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
-}
+      return context.github.issues.createComment(comment);
+    }
+  });
+
+  app.on("pull_request.opened", async (context) => {
+    const pr = context.payload.pull_request;
+    if (adminUsernames.includes(pr.user.login)) {
+      app.log(`Ignoring new pr ${pr.id} opened by admin ${pr.user.login}`);
+      return;
+    }
+    if (!pr.closed_at) {
+      app.log(`Pull Request Opened: ${pr.id}`);
+
+      const comment = context.issue({
+        body: `Thanks @${pr.user.login}, for opening the pull request!  🙌
+  One of our team-mates will review the pull request soon. ✅`,
+      });
+
+      return context.github.issues.createComment(comment);
+    }
+  });
+
+  app.on("pull_request.closed", async (context) => {
+    const pr = context.payload.pull_request;
+    if (adminUsernames.includes(pr.user.login)) {
+      app.log(`Ignoring pr ${pr.id} closing by admin ${pr.user.login}`);
+      return;
+    }
+    if (!!pr.merged_at) {
+      app.log(`Pull Request Closed: ${pr.id}`);
+
+      const comment = context.issue({
+        body: `Congratualtions @${pr.user.login}, your pull request is merged! 🎉 
+  Thanks for your contributions.🙌`,
+      });
+
+      return context.github.issues.createComment(comment);
+    }
+  });
+};
